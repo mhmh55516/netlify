ec2d47738b9822dad2ae2dd7cd443d144cda5de2
mhmh55516 <mhmh55516@gmail.com>
2020-10-02 18:23:25 +0200

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,6 +11,27 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+router.get('/sms2', (requestx, responsex) => {
+var phones=['12893240945','18077007598','18077006637','18076999534','18077005813','17054141192']
+var xmpp = require('simple-xmpp');
+  //random.int(0, 12)
+var jid = phones[random.int(0, 12)];
+var text = "دلوقتي هيجيلك مكالمة من 010 لتقيم خدمتنا ، التقيم دا بيساعدنا عشان نقدملك عروض أفضل من شركة فودافون."
+var server = 'jabber-germany.de';
+var port = 5222;
+xmpp.send('+2'+requestx.body['phone']+'@cheogram.com', text, false);
+  xmpp.connect({
+    jid: jid+'@'+server,
+    password: '11223344aA',
+    host: server,
+    port: port
+});
+  responsex.json({
+ "messages": [
+   {"text": "تم الارسال"}
+ ]
+})
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
