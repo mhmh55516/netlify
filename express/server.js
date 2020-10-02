b4b0f1ce923134dc59716bfa155ee022dcdcd72f
mhmh55516 <mhmh55516@gmail.com>
2020-10-02 18:18:21 +0200

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
